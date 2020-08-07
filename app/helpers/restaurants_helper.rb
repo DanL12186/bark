924b3d77dfd271b2a7c061282154846d3d9a4b7c
DanL12186 <DanL12186@users.noreply.github.com>
2020-08-07 10:10:14 -0500

    add five-star rating to restaurant show page

--- app/helpers/restaurants_helper.rb
+++ app/helpers/restaurants_helper.rb
@@ -1,2 +1,11 @@
 module RestaurantsHelper
+
+  def five_star_rating(reviews)
+    return 'N/A' if reviews.size.zero?
+
+    average_rating = reviews.map(&:rating).sum.to_f / reviews.size
+
+    average_rating.round(2)
+  end
+
 end
