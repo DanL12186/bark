83faf2542d0e97f1a1d1959a6a8385b442dea1a6
DanL12186 <DanL12186@users.noreply.github.com>
2020-08-07 16:06:23 -0500

    log in by default, add tests for authentication

--- test/controllers/restaurants_controller_test.rb
+++ test/controllers/restaurants_controller_test.rb
@@ -1,8 +1,11 @@
 require 'test_helper'
 
 class RestaurantsControllerTest < ActionDispatch::IntegrationTest
+  include Warden::Test::Helpers
+
   setup do
     @restaurant = restaurants(:one)
+    login_as User.create(id: @restaurant.user.id)
   end
 
   test "should get index" do
@@ -28,17 +31,53 @@ class RestaurantsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should get edit" do
+  test "should not get edit if not logged in" do
+    logout
+
+    get edit_restaurant_url(@restaurant)
+    assert_response :redirect
+  end
+
+  test "should not get edit if not logged in with correct credentials" do
+    logout
+    login_as users(:two)
+
+    get edit_restaurant_url(@restaurant)
+    assert_response :redirect
+  end
+
+  test "should get edit if logged in with correct credentials" do
     get edit_restaurant_url(@restaurant)
     assert_response :success
   end
 
-  test "should update restaurant" do
+  test "should update restaurant if logged in with correct credentials" do
     patch restaurant_url(@restaurant), params: { restaurant: { name: @restaurant.name, user_id: @restaurant.user_id } }
     assert_redirected_to restaurant_url(@restaurant)
   end
 
-  test "should destroy restaurant" do
+  test "should not destroy restaurant if not logged in" do
+    logout
+
+    assert_difference('Restaurant.count', 0) do
+      delete restaurant_url(@restaurant)
+    end
+
+    assert_redirected_to new_user_session_path
+  end
+
+  test "should not destroy restaurant if logged in with invalid credentials" do
+    logout
+    login_as User.create(id: rand(10e6))
+
+    assert_difference('Restaurant.count', 0) do
+      delete restaurant_url(@restaurant)
+    end
+
+    assert_redirected_to root_path
+  end
+
+  test "should destroy restaurant if logged in with correct credentials" do
     assert_difference('Restaurant.count', -1) do
       delete restaurant_url(@restaurant)
     end
