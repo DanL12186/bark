1cb7a04586bf432398f983c594cc98b937b18864
DanL12186 <DanL12186@users.noreply.github.com>
2020-08-08 14:59:52 -0500

    add review model tests

--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class ReviewTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:one)
+    @restaurant = restaurants(:one)
+
+    @valid_review = Review.create(
+      rating: 5,
+      comment: "Great place",
+      user_id: @user.id,
+      restaurant_id: @restaurant.id
+    )
+
+    @blank_review = Review.create(rating: 5, user_id: @user.id, restaurant_id: @restaurant.id)
+
+    @short_review = Review.create(
+      rating: 5,
+      comment: "Great",
+      user_id: @user.id,
+      restaurant_id: @restaurant.id
+    )
+  end
+
+  test 'invalid when comment blank or too short' do
+    assert_not @blank_review.valid?, 'user is valid without a password'
+    assert @short_review.errors.messages[:comment].any? { | msg | msg.match?('too short') }
+  end
+
+  test 'valid review' do
+    assert @valid_review.valid?
+  end
 end
