83bfae9dd1742f3953c02c7e5ecc2029e73366d0
DanL12186 <DanL12186@users.noreply.github.com>
2020-08-09 11:43:36 -0500

    add and test validations on restaurant model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,8 @@
 class Restaurant < ApplicationRecord
   belongs_to :user
-  has_many :reviews
 
+  has_many :reviews
   has_many_attached :photos
+
+  validates :name, :user_id, presence: true
 end
--- test/models/restaurant_test.rb
+++ test/models/restaurant_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class RestaurantTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @valid_restaurant = restaurants(:one)
+    @restaurant_without_name = Restaurant.new(user_id: 4)
+    @restaurant_without_user_id = Restaurant.new(name: "Spartacus")
+  end
+
+  test 'invalid without name' do
+    assert_not @restaurant_without_name.valid?
+  end
+
+  test 'invalid without user_id' do
+    assert_not @restaurant_without_user_id.valid?
+  end
+
+  test 'valid restaurant' do
+    assert @valid_restaurant.valid?
+  end
 end
