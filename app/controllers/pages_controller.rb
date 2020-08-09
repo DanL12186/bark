39b6530da9b4abc9e9075724ee4e096eb98fd80e
DanL12186 <DanL12186@users.noreply.github.com>
2020-08-09 11:32:46 -0500

    add comment, place action on single line

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,12 @@
 class PagesController < ApplicationController
   include RestaurantsHelper
 
+  #couldn't work out a way to order restaurants by average rating which both played well with pagination 
+  #and displayed restaurants that didn't have reviews, and simply paginating after an AR Collection had been coerced into an array 
+  #by calling #sort_by felt as though it nearly defeated the purpose of pagination performance-wise. 
+
   def home
-    @restaurants = Restaurant.includes(:reviews)
-                             .all
-                             .sort_by { | restaurant | -five_star_rating(restaurant.reviews) }
+    @restaurants = Restaurant.includes(:reviews).sort_by { | restaurant | -five_star_rating(restaurant.reviews) }
   end
 
 end
