c3133b59804f7e14fb2c0082b8af46b7c301a12a
DanL12186 <DanL12186@users.noreply.github.com>
2020-08-12 10:09:05 -0500

    only allow review owners to destroy own reviews
    don’t allow restaurant owners to submit a form via HTML to review own
    restaurants

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,32 +3,41 @@ class ReviewsController < ApplicationController
 
   def create
     @restaurant = Restaurant.includes(:user).find(params[:restaurant_id])
-    @review = @restaurant.reviews.new(params[:review].permit(:rating, :comment))
-    @review.user = current_user
-    @review.photos.attach(params[:review][:photo])
-    @review.save
 
-    if @review.valid?
-      review_details = { 
-        user: @restaurant.user, 
-        reviewer: current_user, 
-        restaurant: @restaurant, 
-        review: @review, 
-        photo_count: @review.photos.size 
-      }
-      
-      #I didn't explicitly integrate this into ActiveJob because it does this automatically and is async with .deliver_later
-      UserMailer.with(review_details).review_notification_email.deliver_later
+    if @restaurant.user == current_user
+      redirect_to root_path
+    else
+      @review = @restaurant.reviews.new(params[:review].permit(:rating, :comment))
+      @review.user = current_user
+      @review.photos.attach(params[:review][:photo])
+      @review.save
+
+      if @review.valid?
+        review_details = { 
+          user: @restaurant.user, 
+          reviewer: current_user, 
+          restaurant: @restaurant, 
+          review: @review, 
+          photo_count: @review.photos.size 
+        }
+        
+        #I didn't explicitly integrate this into ActiveJob because it does this automatically and is async with .deliver_later
+        UserMailer.with(review_details).review_notification_email.deliver_later
+      end
     end
 
     redirect_to restaurant_path(@restaurant)    
   end
 
   def destroy
-    @restaurant = Restaurant.find(params[:restaurant_id])
+    @restaurant = Restaurant.includes(:review).find(params[:restaurant_id])
     @review = @restaurant.reviews.find(params[:id])
-    
-    @review.destroy
-    redirect_to restaurant_path(@restaurant)
+
+    unless @review.user == current_user
+      redirect_to root_path
+    else
+      @review.destroy
+      redirect_to restaurant_path(@restaurant)
+    end
   end
 end
