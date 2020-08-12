4bf541032ed0672bda21ac5e6f86d97cbd402a2c
DanL12186 <DanL12186@users.noreply.github.com>
2020-08-12 00:00:43 -0500

    Properly query and paginate restaurants

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,14 @@
 class PagesController < ApplicationController
   include RestaurantsHelper
-
-  #couldn't work out a way to order restaurants by average rating which both played well with pagination 
-  #and displayed restaurants that didn't have reviews, and simply paginating after an AR Collection had been coerced into an array 
-  #by calling #sort_by felt as though it nearly defeated the purpose of pagination performance-wise. 
+  include Pagy::Backend
 
   def home
-    @restaurants = Restaurant.includes(:reviews).sort_by { | restaurant | -five_star_rating(restaurant.reviews) }
+    @pagy, @restaurants = pagy(
+      Restaurant.includes(:reviews)
+                .left_outer_joins(:reviews)
+                .group(:restaurant_id, :name)
+                .order('AVG(reviews.rating) DESC')
+    )
   end
 
 end
