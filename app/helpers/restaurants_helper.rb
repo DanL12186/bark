09700b9536d01bbc2bab8e4d4c206fee55fed839
DanL12186 <DanL12186@users.noreply.github.com>
2020-08-12 00:09:11 -0500

    minor refactor

--- app/helpers/restaurants_helper.rb
+++ app/helpers/restaurants_helper.rb
@@ -4,7 +4,7 @@ module RestaurantsHelper
   def five_star_rating(reviews)
     return 0 if reviews.size.zero?
 
-    average_rating = reviews.map(&:rating).sum.to_f / reviews.size
+    average_rating = reviews.map(&:rating).sum.fdiv(reviews.size)
 
     average_rating.round(2)
   end
